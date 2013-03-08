22657333934ac75add64c8f7779e4faa7e462d88
Justin <yuting1987@gmail.com>
2013-03-08 15:20:16 +0800

    Update

--- dev/coffee/index.coffee
+++ dev/coffee/index.coffee
@@ -6,7 +6,7 @@ do ->
   ga = document.createElement('script')
   ga.type = 'text/javascript'
   ga.async = true
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
+  ga.src = (if 'https:' == document.location.protocol then 'https://ssl' else 'http://www') + '.google-analytics.com/ga.js'
   s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(ga, s)
 
--- script/index.js
+++ script/index.js
@@ -8,13 +8,11 @@ _gaq.push(['_setAccount', 'UA-37161265-5']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga, s, _ref;
+  var ga, s;
   ga = document.createElement('script');
   ga.type = 'text/javascript';
   ga.async = true;
-  ga.src = ((_ref = 'https:' === document.location.protocol) != null ? _ref : {
-    'https://ssl': 'http://www'
-  }) + '.google-analytics.com/ga.js';
+  ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   s = document.getElementsByTagName('script')[0];
   return s.parentNode.insertBefore(ga, s);
 })();
